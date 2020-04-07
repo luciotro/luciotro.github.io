46e5aea8eb6a15a366a6140105e754c8c1c78784
Lucio Troiani <lucio.troiani@live.com>
2020-04-07 18:55:50 -0400

    Update places.js

--- names/places.js
+++ names/places.js
@@ -70,7 +70,7 @@ window.onload = () => {
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     text.setAttribute('title', place.name);
                     text.setAttribute('href', 'http://www.example.com/');
-                    text.setAttribute('scale', '13 13 13');
+                    text.setAttribute('scale', '33 33 33');
 
                     text.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
