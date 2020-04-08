84ba47f6c8af2f16ac84e8aacf23103fcbc5aa5f
Lucio Troiani <lucio.troiani@live.com>
2020-04-07 22:57:19 -0400

    Update places.js

--- names/places.js
+++ names/places.js
@@ -52,39 +52,39 @@ function loadPlaceFromAPIs(position) {
 };
 
 
-window.onload = () => {
-    const scene = document.querySelector('a-scene');
-
-    // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
-
-        // than use it to load from remote APIs some places nearby
-        loadPlaces(position.coords)
-            .then((places) => {
-                places.forEach((place) => {
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
-
-                    // add place name
-                    const text = document.createElement('a-link');
-                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    text.setAttribute('title', place.name);
-                    text.setAttribute('href', 'http://www.example.com/');
-                    text.setAttribute('scale', '63 63 63');
-
-                    text.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
-
-                    scene.appendChild(text);
-                });
-            })
-    },
-        (err) => console.error('Error in retrieving position', err),
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 27000,
-        }
-    );
-};
+// window.onload = () => {
+//     const scene = document.querySelector('a-scene');
+//
+//     // first get current user location
+//     return navigator.geolocation.getCurrentPosition(function (position) {
+//
+//         // than use it to load from remote APIs some places nearby
+//         loadPlaces(position.coords)
+//             .then((places) => {
+//                 places.forEach((place) => {
+//                     const latitude = place.location.lat;
+//                     const longitude = place.location.lng;
+//
+//                     // add place name
+//                     const text = document.createElement('a-link');
+//                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+//                     text.setAttribute('title', place.name);
+//                     text.setAttribute('href', 'http://www.example.com/');
+//                     text.setAttribute('scale', '63 63 63');
+//
+//                     text.addEventListener('loaded', () => {
+//                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+//                     });
+//
+//                     scene.appendChild(text);
+//                 });
+//             })
+//     },
+//         (err) => console.error('Error in retrieving position', err),
+//         {
+//             enableHighAccuracy: true,
+//             maximumAge: 0,
+//             timeout: 27000,
+//         }
+//     );
+// };
